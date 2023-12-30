7e71b993f79b62711e1a0370684cf5108434a690
Guyzie754 <67240493+ghazie754@users.noreply.github.com>
2023-12-30 21:35:19 +0000

    fuuzy3

--- sketch.js
+++ sketch.js
@@ -1,32 +1,31 @@
 let Shaderfile;
-let frameCount = 60
+let frameCount = 60;
 let position = 0;
-let img ;
+let img;
 let slider;
 function preload() {
-  Shaderfile = loadShader('Shaders/shader.vert','Shaders/shader.frag');
+  Shaderfile = loadShader("Shaders/shader.vert", "Shaders/shader.frag");
 }
 function setup() {
-  let img =createCanvas(windowWidth,windowHeight,WEBGL);
+  let img = createCanvas(windowWidth, windowHeight, WEBGL);
   noStroke();
- Shaderfile.setUniform('resolution', [width, height]); 
-     }
-function draw() { 
+  Shaderfile.setUniform("resolution", [width, height]);
+}
+function draw() {
+  // shader() sets the active shader with our shader
 
-    // shader() sets the active shader with our shader 
-    
   let mx = map(mouseX, 0, width, 0, 1);
   let my = map(mouseY, 0, height, 0, 1);
-  
-    // lets send the time and resolution to our shader 
-   
-  Shaderfile.setUniform('time', frameCount * 0.1); 
-  Shaderfile.setUniform('mouse', [mx, my]);
-   
-  shader(Shaderfile);  
-  clear()  
-    // rect gives us some geometry on the screen 
-  rect(0,0,width, height);
+
+  // lets send the time and resolution to our shader
+
+  Shaderfile.setUniform("time", frameCount * 0.1);
+  Shaderfile.setUniform("mouse", [mx, my]);
+
+  shader(Shaderfile);
+  clear();
+  // rect gives us some geometry on the screen
+  rect(0, 0, width, height);
 }
 
 function windowResized() {
@@ -37,9 +36,8 @@ function mousePressed() {
   if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
     let fs = fullscreen();
     fullscreen(!fs);
-    
   }
 }
 function doubleClicked() {
-  saveCanvas(img,'jpg'); 
+  saveCanvas(img, "jpg");
 }
